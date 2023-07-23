4662d234e5ddb16af6a74db00a15307594a1ad61
zilinR <zilinrong@kaito.com>
2023-07-23 19:11:02 +0800

    feat: update

--- app/api/imagine/route.ts
+++ app/api/imagine/route.ts
@@ -3,39 +3,53 @@ import { NextRequest } from "next/server";
 export const runtime = "edge";
 
 export const POST = async (req: NextRequest) => {
-  const client = await getClient();
-  const { prompt } = await req.json();
+  try {
+    console.log("imagine.start");
 
-  // const prompt =
-  //   "Christmas dinner with spaghetti with family in a cozy house, we see interior details , simple blue&white illustration";
-  const encoder = new TextEncoder();
-  const readable = new ReadableStream({
-    start(controller) {
-      console.log("imagine.start", prompt);
-      client
-        .Imagine(prompt, (uri: string, progress: string) => {
-          console.log("imagine.loading", uri);
-          controller.enqueue(encoder.encode(JSON.stringify({ uri, progress })));
-        })
-        .then((msg) => {
-          console.log("imagine.done", msg);
-          controller.enqueue(encoder.encode(JSON.stringify(msg)));
-          client.Close();
-          controller.close();
-        })
-        .catch((err: any) => {
-          console.log("imagine.error", err);
-          client.Close();
-          controller.close();
-        });
-    },
-  });
-  return new Response(readable, {
-    headers: {
-      "Content-Type": "text/event-stream",
-      "Access-Control-Allow-Origin": "*",
-      Connection: "keep-alive",
-      "Cache-Control": "no-cache, no-transform",
-    },
-  });
+    const client = await getClient();
+    const { prompt } = await req.json();
+    console.log("imagine.start");
+    // const prompt =
+    //   "Christmas dinner with spaghetti with family in a cozy house, we see interior details , simple blue&white illustration";
+    const encoder = new TextEncoder();
+    const readable = new ReadableStream({
+      start(controller) {
+        console.log("imagine.start", prompt);
+        client
+          .Imagine(prompt, (uri: string, progress: string) => {
+            console.log("imagine.loading", uri);
+            controller.enqueue(
+              encoder.encode(JSON.stringify({ uri, progress }))
+            );
+          })
+          .then((msg) => {
+            console.log("imagine.done", msg);
+            controller.enqueue(encoder.encode(JSON.stringify(msg)));
+            client.Close();
+            controller.close();
+          })
+          .catch((err: any) => {
+            console.log("imagine.error", err);
+            client.Close();
+            controller.close();
+          });
+      },
+    });
+    return new Response(readable, {
+      headers: {
+        "Content-Type": "text/event-stream",
+        "Access-Control-Allow-Origin": "*",
+        Connection: "keep-alive",
+        "Cache-Control": "no-cache, no-transform",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response("error", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        Connection: "keep-alive",
+      },
+    });
+  }
 };
